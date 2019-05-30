ddf67f1681a53ceff523d1394582d13b0234e129
ASanchez-Tojar <asanchez@orn.mpg.de>
2019-05-30 10:39:41 +0200

    First commit

---
+++ 001_literature_screening.R
@@ -0,0 +1,39 @@
+###################################################################
+# Authors: 
+# Alfredo Sanchez-Tojar
+
+# Script first created in May 2019
+
+###################################################################
+# Description of script and Instructions
+###################################################################
+
+# This script is to format a reference dataset so that it can be
+# imported into rayyan for title-and-abstract screening. This data 
+# is part of the following study:
+
+# Antica Culina, Ilona van den Berga, Simon Evans, Alfredo Sánchez-Tójar. 
+# Low availability of code in ecology: a call for urgent action.
+
+
+###################################################################
+# Packages needed
+##############################################################
+
+pacman::p_load(revtools)
+
+# Clear memory
+rm(list=ls())
+
+
+##############################################################
+# Functions needed
+##############################################################
+
+# none
+
+
+##############################################################
+# Importing data
+##############################################################
+
