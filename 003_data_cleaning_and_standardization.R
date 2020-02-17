6b500041b033659083a04e210ab0da4a14040044
Alfredo Sanchez-Tojar <alfredo.tojar@gmail.com>
2020-02-17 11:09:23 +0100

    adding dois, polishing things

--- 003_data_cleaning_and_standardization.R
+++ 003_data_cleaning_and_standardization.R
@@ -40,11 +40,11 @@ rm(list=ls())
 ##############################################################
 
 # importing data for 2016-2017
-db.2016.17 <- read.xlsx("data/Data_Feb_2020_V2.xlsx",
+db.2016.17 <- read.xlsx("data/Data_Feb_2020_V4.xlsx",
                         colNames = T,sheet = 1)
 
 # importing data for 2016-2017
-db.2018.19 <- read.xlsx("data/Data_Feb_2020_V2.xlsx",
+db.2018.19 <- read.xlsx("data/Data_Feb_2020_V4.xlsx",
                         colNames = T ,sheet = 2)
 
 
@@ -87,12 +87,13 @@ names(db.full)
 ###############################
 # Journal
 table(db.full$Journal)
+sum(table(db.full$Journal))
 
 # # exporting journal names for finding updated impact factor
 # write.csv(unique(db.full[,c("Journal","Impact_Factor")]),"data/journal_if.csv",
 #           row.names=FALSE)
 
-# visualizing where the NA's are
+# visualizing where the NA's are, if any: none, all fixed
 db.full[is.na(db.full$Journal),]
 
 
@@ -100,10 +101,7 @@ db.full[is.na(db.full$Journal),]
 # Publication_year
 table(db.full$Publication_year)
 
-# there is a typo in one year of publication: 2916 should be 2016
-db.full[!(is.na(db.full$Publication_year)) & db.full$Publication_year==2916,"Publication_year"] <- "2016"
-
-# furthermore there are three NA's that need fixing
+# check if there are NA's: none, all fixed
 db.full[is.na(db.full$Publication_year),]
 
 # we are creating a variable with two levels to properly label the two 
@@ -113,6 +111,8 @@ db.full$Publication_year.2 <- ifelse(db.full$Publication_year < 2017,
                                      "2018-2019")
 
 db.full$Publication_year.2 <- as.factor(db.full$Publication_year.2)
+table(db.full$Publication_year.2)
+
 
 ###############################
 # Impact_Factor
@@ -127,6 +127,7 @@ impact.factor <- read.table("data/journal_if_WoS_20200206.csv",header=T,sep=",")
 db.full <- db.full[ , -which(names(db.full) %in% c("Impact_Factor"))]
 db.full <- merge(db.full,impact.factor,by="Journal",all.x=TRUE)
 table(db.full$Impact_Factor)
+sum(table(db.full$Impact_Factor))
 
 
 ###############################
@@ -153,9 +154,8 @@ names(db.full)[7] <- "statistical.analysis.and.or.simulations"
 
 table(db.full$statistical.analysis.and.or.simulations)
 
-# for those named as "yes, implement a model" we are going to add this information
-# to the variable "additional.comment.on.analysis" and rename them as "yes, simulation"
-
+# for those named as "yes, implement a model" we are going rename them as "yes, simulation" 
+# (but the information will still be available in the additional.comment.on.analysis)
 db.full[!(is.na(db.full$statistical.analysis.and.or.simulations)) & 
           db.full$statistical.analysis.and.or.simulations=="yes, implement a model",]
 
@@ -167,13 +167,15 @@ db.full$statistical.analysis.and.or.simulations <- recode(db.full$statistical.an
                                                           "yes, methodological and statistical" = "yes, statistical and methodological",
                                                           .default = levels(db.full$statistical.analysis.and.or.simulations))
 
+table(db.full$statistical.analysis.and.or.simulations)
 
-# creating a new variable with simply yes or no to make subsetting easier
+# creating a new variable with simply yes or no to make subsetting easier, also, becuase some papers are dificult to label (e.g. simulation vs. model),
+# and we are not interesting in labelling them per se, just to know if they provide some stats or simulations for which code could be available.
 db.full$statistical.analysis.and.or.simulations.2 <- as.factor(ifelse(as.character(db.full$statistical.analysis.and.or.simulations)=="no",
                                                                       "no",
                                                                       "yes"))
 
-table(db.full$statistical.analysis.and.or.simulations.2 )
+table(db.full$statistical.analysis.and.or.simulations.2)
 
 # checking consistency in data collection. For those papers that we reviewed,
 # if statistical.analysis.and.or.simulations.2 == "no", there should not be data collected 
@@ -245,8 +247,6 @@ db.full$CodePublished <- factor(db.full$CodePublished)
 db.full$CodePublished.2 <- factor(db.full$CodePublished.2)
 table(db.full$CodePublished.2)
 
-
-
 # checking consistency in data collection. For those papers with some code published,
 # there should be data collected about the code 
 summary(db.full[!(is.na(db.full$CodePublished.2)) & 
@@ -258,7 +258,8 @@ summary(db.full[!(is.na(db.full$CodePublished.2)) &
 
 # this is an interesting case where code is provided only as an R package, 
 # but not the code to reproduce the simulation, that is why we call it
-# CodePublished="no", and have to make CodeMentioned and Location_CodeMentioned as NA
+# CodePublished="no", and we are going to make CodeMentioned and Location_CodeMentioned as NA
+# to keep things simple
 db.full[!(is.na(db.full$CodePublished.2)) & 
           db.full$CodePublished.2=="no" & 
           !(is.na(db.full$CodeMentioned)),
@@ -330,12 +331,13 @@ db.full$FreeSoftware <- recode(db.full$FreeSoftware,
 levels(db.full$FreeSoftware)[levels(db.full$FreeSoftware)=='NA'] <- NA
 
 
-# creating new, recoded variable
+# creating new, recoded variable were for those for which at least some software is free, we label them as FreeSoftware=="yes"
 db.full$FreeSoftware.2 <- recode(db.full$FreeSoftware,
                                  "partially" = "yes",
                                  .default = levels(db.full$FreeSoftware))
 
 table(db.full$FreeSoftware.2)
+table(db.full[db.full$CodePublished.2=="yes","FreeSoftware.2"])
 
 
 ###############################
@@ -372,6 +374,15 @@ db.full$DataShared.3 <- recode(db.full$DataShared.2,
                                .default = levels(db.full$DataShared.2))
 
 table(db.full$DataShared.3)
+# table(db.full[db.full$statistical.analysis.and.or.simulations.2=="yes" & !(is.na(db.full$statistical.analysis.and.or.simulations.2)),"DataShared.3"])
+# nrow(db.full[db.full$statistical.analysis.and.or.simulations.2=="yes" & !(is.na(db.full$statistical.analysis.and.or.simulations.2)) & is.na(db.full$DataShared.3),])
+# table(db.full[db.full$statistical.analysis.and.or.simulations.2=="yes" & !(is.na(db.full$statistical.analysis.and.or.simulations.2)) & is.na(db.full$DataShared.3),"DataUsed"])
+
+nrow(db.full[db.full$CodePublished.2=="yes" & db.full$DataShared.3=="yes" &
+               !(is.na(db.full$CodePublished.2)) & !(is.na(db.full$DataShared.3)),])
+
+nrow(db.full[db.full$CodePublished.2=="yes" & db.full$DataShared.3=="yes" & db.full$FreeSoftware.2=="yes" &
+               !(is.na(db.full$CodePublished.2)) & !(is.na(db.full$DataShared.3)) & !(is.na(db.full$FreeSoftware.2)),])
 
 
 ###############################
@@ -380,12 +391,73 @@ table(db.full$Second_screener)
 
 
 ###############################
+# adding doi's
+# from the title and abstract screening database
+doi.2018.19 <- read.table("screening_process/title-and-abstract_screening/random_200_2018_2019_rayyan.csv",header=T,sep=",")
+
+# reducing database
+doi.2018.19.red <- doi.2018.19[,c("title","url")]
+names(doi.2018.19.red) <- c("Title","doi")
+
+# removing {} to make matchin easier
+doi.2018.19.red$Title <- str_remove_all(doi.2018.19.red$Title, "[{}]")
+db.full$Title <- str_remove_all(db.full$Title, "[{}]")
+
+
+# adding dois by merging by title
+# first, are the titles the same (i.e. format is the same, no need to worry)
+setdiff(db.full[db.full$Publication_year>2017,"Title"],doi.2018.19.red$Title)
+setdiff(doi.2018.19.red$Title,db.full[db.full$Publication_year>2017,"Title"])
+
+# everything matches, therefore I can add dois to the database
+db.full.doi <- merge(db.full,doi.2018.19.red,by="Title",all.x=T)
+
+# adding a missing doi manually
+db.full.doi$doi <- as.character(db.full.doi$doi)
+db.full.doi[db.full.doi$fulltextID=="CAE168" & !(is.na(db.full.doi$fulltextID)),"doi"]<-"10.1002/ecy.2191"
+
+# adding doi's from before 2017
+db.full.doi$fulltextID <- as.character(db.full.doi$fulltextID)
+db.full.doi$doi <- ifelse(is.na(db.full.doi$doi),
+                          db.full.doi$fulltextID,
+                          db.full.doi$doi)
+
+db.full.doi$doi <- as.factor(db.full.doi$doi)
+summary(db.full.doi)
+
+
+###############################
 # exporting clean dataset
-write.csv(db.full,"data/code_availability_full_and_clean.csv",
+
+db.full.doi.reduced <- db.full.doi[,c("doi","Journal","Publication_year","Publication_year.2",
+                                      "Excluded.abstract.screening","statistical.analysis.and.or.simulations.2",
+                                      "bioinformatic.analysis","additional.comment.on.analysis",
+                                      "Stat_analysis_software","CodePublished","CodePublished.2","LinktoCode",
+                                      "BreafDescription","InlineComments",
+                                      "CodeMentioned","CodeMentioned.2","LocationShared","LocationShared.2",
+                                      "Repository","FreeSoftware","DataUsed","DataShared.2","DataShared.3",
+                                      "ExtraComments","Second_screener","Changes_after_second_screening",
+                                      "Remarks_on_decisions")]
+
+write.csv(db.full.doi.reduced,"data/code_availability_full_and_clean.csv",
           row.names=FALSE)
 
 
-# checking papers for which only simulations where run to see if CodePublished DataUsed and DataShared agree
-db.full[!(is.na(db.full$statistical.analysis.and.or.simulations)) & 
-          db.full$statistical.analysis.and.or.simulations=="yes, simulation",
-        c("fulltextID","Authors","Publication_year","CodePublished","DataUsed","DataShared","Second_screener")]
+#
+#
+#
+# double checking consistency for those studies with simulations and code, data should be at least partially shared (depends, though, think about it) (if the code provided generates the data)
+# db.full[db.full$statistical.analysis.and.or.simulations=="yes, simulation" & 
+#           !(is.na(db.full$statistical.analysis.and.or.simulations)) & 
+#           db.full$CodePublished=="yes" & db.full$DataUsed=="yes",
+#         c("statistical.analysis.and.or.simulations","CodePublished","DataUsed","DataShared")]
+# db.full[db.full$statistical.analysis.and.or.simulations=="yes, statistical and simulation" & 
+#           !(is.na(db.full$statistical.analysis.and.or.simulations)) &
+#           db.full$CodePublished!="no",
+#         c("statistical.analysis.and.or.simulations","CodePublished","DataUsed","DataShared")]
+
+
+# # checking papers for which only simulations where run to see if CodePublished DataUsed and DataShared agree
+# db.full[!(is.na(db.full$statistical.analysis.and.or.simulations)) & 
+#           db.full$statistical.analysis.and.or.simulations=="yes, simulation",
+#         c("fulltextID","Authors","Publication_year","CodePublished","DataUsed","DataShared","Second_screener")]
